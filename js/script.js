496bb275c572a8e6cb3af783fbbcb405641075f3
sajununaidu <sajununaidu1@gmail.com>
2025-05-17 21:49:49 +0530

    Made required changes for refactoting code

--- js/script.js
+++ js/script.js
@@ -44,8 +44,6 @@ function sendMail(){
 		message = message.replaceAll("\r", "%0D");
 		message = message.replaceAll("\n", "%0A");
 		message = message.replaceAll("\t", "%09");
-	//	https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=target@email.com&subject=MISSED%20CALL%20EZTRADER&body=Hello%2C%0A%0AI%20tried%20contacting%20you%20today%20but%20you%20seem%20to%20have%20missed%20my%20call.%20%0A%0APlease%20return%20my%20call%20as%20soon%20as%20you%E2%80%99re%20available.%20%0A%0AIn%20any%20case%2C%20I%20will%20try%20ringing%20you%20at%20a%20later%20time.%0A%0A%0ATy%2C%0A%0A%0A%0A
-
 		window.location.href = "https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=tree2boxinteriors@gmail.com&subject="+subject+"&body="+"Hi%20tree2box"+message+"%0ABest%20regards%0A"+name;
 	}
 }
